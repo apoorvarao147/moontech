fe573f843d7f4a6ffc1015e7e1854f699030a0a5
Apoorva Rao <apoorvarao147@gmail.com>
2023-11-16 18:28:39 -0500

    created ContactUs and Services pages

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import "./styles/App.scss";
 import Header from "./components/Header";
 import Home from "./components/Home";
 import Footer from "./components/Footer";
+import Contact from "./components/Contact";
+import Services from "./components/Services";
 
 function App() {
   return (
@@ -10,6 +12,8 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/services" element={<Services />} />
       </Routes>
       <Footer />
     </Router>
